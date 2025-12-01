3762302fd3f76a05845e18d694b9f9aeb2b695fb
henriquelaia <152277818+henriquelaia@users.noreply.github.com>
2025-12-01 22:26:27 +0000

    feat: add `commentId` to `Attachment` table to link attachments to comments.

---
+++ server/prisma/migrations/20251201222540_add_attachments/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Attachment" ADD COLUMN     "commentId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
