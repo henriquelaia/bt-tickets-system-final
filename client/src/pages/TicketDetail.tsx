3cda384b7ba2178e29a95cbc23f244dbcbbd5771
henriquelaia <152277818+henriquelaia@users.noreply.github.com>
2025-12-02 12:33:51 +0000

    error fixing

--- client/src/pages/TicketDetail.tsx
+++ client/src/pages/TicketDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../lib/api';
 import { useAuth } from '../context/AuthContext';
 import { useSocket } from '../context/SocketContext';
